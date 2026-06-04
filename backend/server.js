928578abb3c4dab660ab5fb2e62ff5d76a4e70ec
sonkarayush <a.sonkar@iitg.ac.in>
2026-06-04 14:24:18 +0530

    Fix: Add CORS policy to server.js

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import path from "path";
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
+import cors from "cors";
 
 import authRoutes from "./routes/auth.routes.js";
 import messageRoutes from "./routes/message.routes.js";
@@ -16,6 +17,10 @@ const __dirname = path.resolve();
 // PORT should be assigned after calling dotenv.config() because we need to access the env variables. Didn't realize while recording the video. Sorry for the confusion.
 const PORT = process.env.PORT || 5000;
 
+app.use(cors({
+  origin: "https://vibe-chat-of6e.vercel.app",
+  credentials: true
+}));
 app.use(express.json()); // to parse the incoming requests with JSON payloads (from req.body)
 app.use(cookieParser());
 
