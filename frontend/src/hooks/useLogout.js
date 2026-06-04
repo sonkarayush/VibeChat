69b43eb305b0d745966a6468090ccaa4465d73fd
sonkarayush <a.sonkar@iitg.ac.in>
2026-06-04 13:50:49 +0530

    Connect frontend hooks to Render

--- frontend/src/hooks/useLogout.js
+++ frontend/src/hooks/useLogout.js
@@ -9,7 +9,7 @@ const useLogout = () => {
 	const logout = async () => {
 		setLoading(true);
 		try {
-			const res = await fetch("/api/auth/logout", {
+			const res = await fetch("https://vibechat-backend-c4lw.onrender.com", {
 				method: "POST",
 				headers: { "Content-Type": "application/json" },
 			});
